f5f3e1583199ed24829110e0717f6392fc40ee05
Kandi <kandirbrts@gmail.com>
2023-07-13 14:38:44 -0500

    commented explanations

--- js/countToTen.js
+++ js/countToTen.js
@@ -1,7 +1,12 @@
+
+//the countToTen function does the following things:
+
 function countToTen(){
-    let count = 0;
-    while (count < 10) {
-        count++;
-        document.getElementById("theCount").innerHTML += count + "<br>";
+    let count = 0; // initializes a variable called count
+
+    while (count < 10) //starts a loop by checking the value of count to make sure it's less than ten
+     {
+        count++; // adds one to the value of the count
+        document.getElementById("theCount").innerHTML += count + "<br>"; //appends the current value of the count, followed by a 'line break', to the paragraph with the id= 'theCount', and starts the loop over
     }
 }
